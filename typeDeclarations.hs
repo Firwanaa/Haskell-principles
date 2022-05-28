85c5eb4d57cedd465e457706327a5b81413674e6
firwanaa <firwanaa@sheridancollege.ca>
2022-05-27 21:31:05 -0400

    rename files

---
+++ typeDeclarations.hs
@@ -0,0 +1,11 @@
+-- TypeDeclarations.hs
+module TypeDeclarations where
+
+import Data.List
+import System.IO
+
+addMe :: Int -> Int -> Int
+
+-- functionName param1 param2 = operations (returned value)
+addMe x y = x + y
+main = do
