28f7a9d02a6f86d13ea1e0069d1d2dcfd5ea8e46
firwanaa <firwanaa@sheridancollege.ca>
2022-06-01 00:32:38 -0400

    qsort - Haskell Programming - Graham Hutton

--- CustomeTypeClasses.hs
+++ CustomeTypeClasses.hs
@@ -20,4 +20,9 @@ instance MyEq ShirtSize where
 
 newSize = areEqual M M
         
-
+data Temprature = C Float | F Float deriving (Show, Eq)
+-- instance Eq Temprature where
+--         (==) (C n) (C m) = n == m
+--         (==) (F n) (F m) = n == m
+--         (==) (C c) (F f) = (1.8*c + 32) == f
+--         (==) (F f) (C c) = (1.8*c + 32) == f
---
+++ Qsort.hs
@@ -0,0 +1,15 @@
+--Haskell Programming By Prof Graham Hutton
+-- Qsort.hs
+module Qsort where
+import Data.List
+import System.IO
+import Data.Text.Internal.Fusion.Size (larger)
+
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+                where
+                        smaller = [a | a <- xs, a <= x]
+                        larger  = [b | b <- xs, b > x]
+
+list1 = [2,3,4,1,0 ,5]
+sortedList1 = qsort list1
