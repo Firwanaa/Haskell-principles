18e13dd17e5eea37d8e752de9c0d689c80a699f2
firwanaa <firwanaa@sheridancollege.ca>
2022-06-12 20:58:26 -0400

    add some comments

--- Sequancing.hs
+++ Sequancing.hs
@@ -1,32 +1,41 @@
 -- Sequancing.hs
 -- Programming In Haskell - Ch 10
 {-# LANGUAGE BlockArguments #-}
+
 module Sequancing where
-import           Data.List
-import           System.IO
+
+import Data.Char (toUpper)
+import Data.List
+import System.IO
+
 -- "do" actions had to line up in the same column
-act :: IO (Char,Char)
-act = do x <- getChar
-         getChar -- takes character but does'nt save it. used to discard "Enter" '\n'
-         y <- getChar
-         getChar -- discard another enter '\n'
-         return (x,y)
+act :: IO (Char, Char)
+act = do
+  x <- getChar
+  getChar -- takes character but does'nt save it. used to discard "Enter" '\n'
+  y <- getChar
+  getChar -- discard another enter '\n'
+  return (x, y)
 
 getLine' :: IO String
-getLine' = do x <- getChar
-              if x == '\n' then
-                return []
-              else
-                do xs <- getLine'
-                   return (x:xs)
+getLine' = do
+  x <- getChar
+  if x == '\n'
+    then return []
+    else do
+      xs <- getLine'
+      return (x : xs)
+
 putStr' :: String -> IO ()
 putStr' [] = return ()
-putStr' (x:xs) = do putChar x
-                    putStr' xs
+putStr' (x : xs) = do
+  putChar x
+  putStr' xs
 
 putStrLn' :: String -> IO ()
-putStrLn' xs = do putStr' xs
-                  putChar '\n'
+putStrLn' xs = do
+  putStr' xs
+  putChar '\n'
 
 nameDo :: IO ()
 nameDo = do
@@ -38,8 +47,16 @@ nameDo = do
   putStrLn ("Pleased to meet you, " ++ full ++ "!")
 
 strlen :: IO ()
-strlen = do putStr "Enter a string: "
-            xs <- getLine
-            putStr "The string has "
-            putStr (show (length xs))
-            putStrLn " characters"
+strlen = do
+  putStr "Enter a string: "
+  xs <- getLine
+  putStr "The string has "
+  putStr (show (length xs))
+  putStrLn " characters"
+
+greet :: IO ()
+greet = do
+  putStrLn "What is your name ?"
+  name <- getLine
+  let uname = map toUpper name
+  putStrLn ("Hello " ++ uname)
