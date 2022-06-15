a5fdd7c159decd65885e05822dd74685eac72206
firwanaa <firwanaa@sheridancollege.ca>
2022-06-15 01:12:45 -0400

    show some love to sieve of eratosthenes

--- primes2.hs
+++ primes2.hs
@@ -2,7 +2,7 @@
 -- Prime numbers in a  Sieve Of Eratosthenes way
 primes = sieve [2 ..]
 
-sieve (p : xs) = p : sieve [x | x <- xs, mod x p /= 0]
+sieve (p : xs) = p : sieve [x | x <- xs, mod x p /= 0] -- ❤️
 
 -- to test it
 -- in ghci
