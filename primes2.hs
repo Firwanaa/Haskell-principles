f0c732cc0684afd8a714a52d1ef49841c896c392
firwanaa <firwanaa@sheridancollege.ca>
2022-06-15 01:25:06 -0400

    twin primes with sieve

--- primes2.hs
+++ primes2.hs
@@ -8,3 +8,15 @@ sieve (p : xs) = p : sieve [x | x <- xs, mod x p /= 0] -- ❤️
 -- in ghci
 -- take 10 primes
 -- takeWhile (<50) primes
+
+-- Twin priems: the two primes that differ by two, eg. 3 and 5
+-- me
+-- tp = twinprime [2 ..]
+
+-- twinprime (p : xs) = p : twinprime [x | x <- sieve xs, (x - p) == 2]
+
+-- Prof Graham
+
+twin (x, y) = y == x + 2
+
+twins = filter twin (zip primes (tail primes))
