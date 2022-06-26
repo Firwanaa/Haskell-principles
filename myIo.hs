38d7bce5d5964379f16f2adad0a3660c9158364b
firwanaa <firwanaa@sheridancollege.ca>
2022-06-26 04:49:20 -0400

    more IO action

--- myIo.hs
+++ myIo.hs
@@ -22,3 +22,10 @@ main = do
       putStrLn ("Input: " ++ i)
       main
     else return ()
+
+count :: Int -> Int -> IO ()
+count n m = do
+  putStrLn (show n)
+  if n < m
+    then count (n + 1) m
+    else return ()
