2d5905ffc183ccdc705d15b00e78668aadb36d00
firwanaa <firwanaa@sheridancollege.ca>
2022-07-03 04:01:11 -0400

    either type

---
+++ Either.hs
@@ -0,0 +1,13 @@
+-- Either.hs
+
+module Either where
+
+import Data.Either
+
+-- data SomeData = Left Int | Right String
+
+-- data Either a b = Left a | Right b
+
+type SomeData = Either Int String
+
+-- [Left 1, Right "Hello"] :: SomeData
