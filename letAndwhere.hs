dac3b920cbef820a6725697524cf81c632dd467f
firwanaa <firwanaa@sheridancollege.ca>
2022-07-10 09:11:53 -0400

    more on lambda function

--- letAndwhere.hs
+++ letAndwhere.hs
@@ -1,3 +1,23 @@
--- |
+-- let in
+sumSquareOrSquareSum x y =
+  let sumSquare = (x ^ 2 + y ^ 2)
+      squareSum = (x + y) ^ 2
+   in if sumSquare > squareSum
+        then sumSquare
+        else squareSum
 
-module nil where
+-- overwrite
+overwrite x =
+  let x = 2
+   in let x = 3
+       in let x = 4
+           in x
+-- redfine overwirte using lambdas
+overwrite x =
+  ( \x ->
+      ( \x ->
+          (\x -> x) 4
+      )
+        3
+  )
+    2
